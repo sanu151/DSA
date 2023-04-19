cb3321b1cf7f64ec81eed9e76e0fbd176d9bdee3
Sanu <das.supriyo07@gmail.com>
2023-04-19 07:18:01 +0530

    First Commit

---
+++ 29.Stack_Operation_using_linked_list.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+    int data;
+    struct stack * next;
+}stack;
+
+stack * createNode(int data){
+    stack * new_node = (stack *)malloc(sizeof(stack));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void displayStack(stack * top){
+    int pos = 1;
+    printf("\n***STACK ELEMENTS***\n");
+    while(top != NULL){
+        printf("Element %d : %d\n", pos, top->data);
+        top = top->next;
+        pos++;
+    }
+    printf("\n");
+}
+
+int isEmpty(stack * top){
+    if(top == NULL)
+        return 1;
+    return 0;
+}
+
+stack * push(stack * top, int data){
+    stack * n = createNode(data);
+    if(n == NULL)
+        printf("Stack Overflow !");
+    else{
+        n->next = top;
+        top = n;
+        return top;
+    }
+}
+
+void pop(stack * top){
+    if(isEmpty(top))
+        printf("Stack is Empty!\n");
+    else{
+        stack * x = top;
+        int item = x->data;
+        top = top->next;
+        free(x);
+        printf("Popped item : %d\n", item);
+    }
+}
+
+int main(){
+    stack * top = NULL;
+
+    top = push(top, 12);
+    top = push(top, 2);
+    top = push(top, 34);
+    displayStack(top);
+    pop(top);
+    displayStack(top);
+
+    return 0;
+}
